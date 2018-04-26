5bce7c51f8b4874645c0fb641b6f87ae387b34c4
Florencia Rodriguez <rodriguezflorencia.imei@gmail.com>
2018-04-26 10:51:04 -0300

    Tp2

---
+++ Trabajo_Practico_02/Consigna_02/src/main.cpp
@@ -0,0 +1,36 @@
+//============================================================================
+// Name        : persona1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include "Persona.h"
+#include "Fecha.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	Fecha f1(06,05,1996);
+
+	Persona p1(39533850, "Rodriguez", "Florencia", f1);
+
+	cout<< p1.getApellido()<< ", "<< p1.getApellido()<< endl;
+	cout<< p1.getDni()<< "---> "<< p1.getNacimiento().getDia()<< "/"
+			<< p1.getNacimiento().getMes()<< "/"<< p1.getNacimiento().getAnho()<< endl;
+
+	//-------------------------------------------------------------------------------------
+	//Usando los metodos imput y show
+
+	Persona p2;
+	p2.imput();
+	//p2.show();
+
+	cout<< p2.getApellido()<< ", "<< p2.getApellido()<< endl;
+	cout<< p2.getDni()<< "---> "<< p2.getNacimiento().getDia()<< "/"
+			<< p2.getNacimiento().getMes()<< "/"<< p2.getNacimiento().getAnho()<< endl;
+
+	return 0;
+}
---
+++ Trabajo_Practico_02/Consigna_03/comunidad/src/Comunidad.cpp
@@ -0,0 +1,36 @@
+/*
+ * Comunidad.cpp
+ *
+ *  Created on: 28 mar. 2018
+ *      Author: hp
+ */
+#include "Fecha.h"
+#include "Persona.h"
+#include "Comunidad.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+Persona* Comunidad::getPersonas() {
+	return personas;
+}
+
+Comunidad::Comunidad() {
+	// TODO Auto-generated constructor stub
+	//personas[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+}
+
+void Comunidad::imput() {
+	cout<< "Ingrese 10 personas: "<< endl;
+	for (int i=0; i<10; ++i) {
+
+		personas[i].imput();
+	}
+}
+
+void Comunidad::show() {
+	for (int i=0; i<10; ++i) {
+		personas[i].show();
+	}
+}
