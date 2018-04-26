5e40eb58a0f83f7396745729b41eb649af8c29d7
Florencia Rodriguez <rodriguezflorencia.imei@gmail.com>
2018-04-26 10:11:22 -0300

    .

--- Trabajo_Practico_02/Consigna_03/comunidad/Debug/src/Comunidad.d
+++
@@ -1,8 +0,0 @@
-src/Comunidad.o: ../src/Comunidad.cpp ../src/Fecha.h ../src/Persona.h \
- ../src/Comunidad.h
-
-../src/Fecha.h:
-
-../src/Persona.h:
-
-../src/Comunidad.h:
--- Trabajo_Practico_02/Consigna_03/comunidad/Debug/src/main.d
+++
@@ -1,5 +0,0 @@
-src/main.o: ../src/main.cpp ../src/Persona.h ../src/Fecha.h
-
-../src/Persona.h:
-
-../src/Fecha.h:
